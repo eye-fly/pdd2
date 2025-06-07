2713cfd6c26df47baafb809b2f50556cbc5ac58e
Jck <jacek.muszynski.mail@gmail.com>
2025-06-07 20:29:58 +0200

    final

--- main.tf
+++ main.tf
@@ -14,10 +14,50 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22",  "3000-54057","7077","7078","4040" ,"8080","8081", "2181", "9092", "8888"]
+    ports    = ["22","2049",   "3000-54057","7077","7078","4040" ,"8080","8081", "2181", "9092", "8888"]
   }
 
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_disk" "nfs_disk" {
+  name  = "nfs-disk"
+  type  = "pd-standard"
+  zone  = var.zone
+  size  = 20
+}
+
+resource "google_compute_instance" "nfs_server" {
+  name         = "nfs-server"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  attached_disk {
+    source      = google_compute_disk.nfs_disk.id
+    device_name = "nfs-disk"
+  }
+
+  network_interface {
+    network       = google_compute_network.vpc_network.name
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "debian:${file("/home/pzero/python/pdd/zad2/.ssh/id_ed25519.pub")}"
+  }
+
+  service_account {
+    email  = "default"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  tags = ["nfs"]
 }
 
 resource "google_compute_instance" "nodes" {
@@ -39,6 +79,11 @@ resource "google_compute_instance" "nodes" {
 
   metadata = {
     ssh-keys = "debian:${file("/home/pzero/python/pdd/zad2/.ssh/id_ed25519.pub")}"
+  }
+
+  service_account {
+    email  = "default"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   tags = ["spark", "kafka"]
@@ -64,8 +109,15 @@ resource "google_compute_instance" "driver" {
     ssh-keys = "debian:${file("/home/pzero/python/pdd/zad2/.ssh/id_ed25519.pub")}"
   }
 
+  service_account {
+    email  = "default"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
   tags = ["spark", "driver"]
 }
+
+
 
 output "external_ips" {
   value = [for instance in google_compute_instance.nodes : instance.network_interface[0].access_config[0].nat_ip]
@@ -74,4 +126,8 @@ output "external_ips" {
 
 output "driver_ip" {
   value = google_compute_instance.driver.network_interface[0].access_config[0].nat_ip
+}
+
+output "nfs_internal_ip" {
+  value = google_compute_instance.nfs_server.network_interface[0].network_ip
 }
